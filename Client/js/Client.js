2c85be6e6f9f32d1d55354e1c6c8dd321370abca
lukasz <lukasz.weresz@gmail.com>
2017-12-09 16:16:22 +0100

    poczatki JS

---
+++ Client/js/Client.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", init, false);
+
+function init()
+{
+    //var login = doument.getElementById("login");
+    document.addEventListener("mousemove", getPosition, false);
+}
+
+function getPosition(event)
+{
+    var x = new Number();
+    var y = new Number();
+    var table = document.getElementById("pos_table");
+    var td = table.getElementsByTagName("td");
+
+    if (event.x != undefined && event.y != undefined)
+    {
+        x = event.x;
+        y = event.y;
+    }
+    else // Firefox method to get the position
+    {
+        x = event.clientX;
+        y = event.clientY;
+    }
+
+    td[0].innerHTML = x;
+    td[1].innerHTML = y;
+}
+
+function showTime(){
+    document.getElementById("time").innerHTML = Date();
+}
+
+function login(){
+
+    var login = document.getElementById("username");
+
+    alert();
+
+}
